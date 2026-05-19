32f6672071a130030e0b37473e7cee0cad8b1280
devtechnosys-rahul <devrahul8955@gmail.com>
2026-05-19 18:41:26 +0530

    DD

--- backend/dist/affiliates/destinations.data.d.ts
+++
@@ -1,3 +0,0 @@
-import type { AffiliateDestination } from "./affiliate.types";
-export declare const AFFILIATE_DESTINATIONS: AffiliateDestination[];
-export declare function findDestinationFromText(text: string): AffiliateDestination | null;
