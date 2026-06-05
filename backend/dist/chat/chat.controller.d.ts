9d345046040f58293fa52163f4989fcf20bf7035
devtechnosys-rahul <devrahul8955@gmail.com>
2026-06-05 13:21:24 +0530

    DD

--- backend/dist/chat/chat.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ChatService } from "./chat.service";
-import type { ChatRequest } from "./chat.types";
-export declare class ChatController {
-    private readonly chatService;
-    constructor(chatService: ChatService);
-    createReply(body: ChatRequest): Promise<import("./chat.types").ChatResponse>;
-}
